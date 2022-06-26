955d58dc4d115ef753404db5eacc8341f9373c1c
Arbaz86 <arbaz10042002@gmail.com>
2022-06-26 11:22:08 +0530

    did some changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { getNotes } from "./notes";
-import { masai_logo } from "./assets/Images/msai_logo.png";
+import masai_logo from "./assets/Images/msai_logo.png";
 import "./styles/index.css";
+
+const root = getElementById("root");
+
+let image = document.getElementById("logo");
+image.className = "msai_logo";
+image.src = masai_logo;
 
 getNotes();
